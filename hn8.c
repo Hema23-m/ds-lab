5465a04a723dcf6bcdc9ddaa92a93c2f349aa38a
Hema23-m <hemagowda766@gmail.com>
2026-08-31 11:42:51 +0530

    Add files via upload

---
+++ hn8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char str[] = "spoorthy";
+    int seen[256] = {0};
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (seen[(unsigned char)str[i]]++) {
+            printf("Last repeated character: '%c'\n", str[i]);
+            return 0;
+        }
+    }
+
+    printf("No repeated characters found.\n");
+    return 0;
+}
